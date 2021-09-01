dbb3c370009f88d201680c390dfbbfbb5bb98bbc
thang290298 <68497823+thang290298@users.noreply.github.com>
2021-09-01 20:05:38 +0700

    update

---
+++ KVM/Create-image-openstack/resetpass.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Openlitespeed reset password
+# ThangNV NhanHoa Cloud Team
+# Get info mysql_root_passwd
+old_passwd_mysl=0435626533aA
+# Input from cloud-init
+new_passwd_1=$1
+new_passwd_2=$2
+# Input from random
+# new_passwd_1=$(date +%s | sha256sum | base64 | head -c 16 ; echo)
+# Change password
+mysqladmin --user=root --password=$old_passwd_mysl password $new_passwd_1
+echo -e "admin\n$new_passwd_2\n$new_passwd_2" | /usr/local/lsws/admin/misc/admpass.sh
+# Restart Openlitespeed
+EOF
+# DONE
+
+
