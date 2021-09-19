80136aecde4e0e023e70cdd3e8136e0c4cc2acf9
nguyen thang <thangnv@dcv.vn>
2021-09-20 00:04:54 +0700

    cập nhật sripts reset info

--- KVM/Create-image-openstack/scripts-reset-info/Centos7-OLS-reset-pass.sh
+++ KVM/Create-image-openstack/scripts-reset-info/Centos7-OLS-reset-pass.sh
@@ -12,9 +12,8 @@ new_passwd_2=$2
 mysqladmin --user=root --password=$old_passwd_mysl password $new_passwd_1
 echo -e "admin\n$new_passwd_2\n$new_passwd_2" | /usr/local/lsws/admin/misc/admpass.sh
 sed -i "s|NjZkYWIw|$new_passwd_2|g" /usr/local/lsws/adminpasswd
-sed -i "s|$old_passwd_mysl|$new_passwd_2|g" /usr/local/lsws/password
+sed -i "s|0435626533aA|$new_passwd_2|g" /usr/local/lsws/password
 # Restart Openlitespeed
 systemctl restart lsws
 systemctl restart mysqld
 # DONE
-0962012918tT
