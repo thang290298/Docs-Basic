40da52cb68c1d7adec46a13bb6185bc22a7ea7c4
thang290298 <68497823+thang290298@users.noreply.github.com>
2022-02-08 11:33:07 +0700

    check version zimbra

---
+++ Mail/check_version_zimbra.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+/usr/sbin/vzlist -a | grep running | awk '{print $1}' > /mnt/quyenbx/id.txt
+for line in `cat /mnt/quyenbx/id.txt`
+do
+ip=`/usr/sbin/vzlist -a | grep ${line} | awk '{print $4}'`
+zimbra=`/usr/sbin/vzctl exec ${line} "netstat -tupln" | grep 7071 |awk '{print $4}' | cut -d: -f2`
+
+if [ "$zimbra" = 7071 ];
+then
+version=`/usr/sbin/vzctl exec ${line} "su - zimbra -c 'zmcontrol -v'"`
+echo "VPS $ip zimbra_$version"
+
+else
+continue
+fi
+done
