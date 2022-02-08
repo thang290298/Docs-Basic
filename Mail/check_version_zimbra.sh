c2408909a62f790219e042ee9186c61db7438e7b
thang290298 <68497823+thang290298@users.noreply.github.com>
2022-02-08 13:44:11 +0700

    cập nhật

--- Mail/check_version_zimbra.sh
+++ Mail/check_version_zimbra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-touch /mnt/zimbra/id.txt
-/usr/sbin/vzlist -a | grep running | awk '{print $1}' > /mnt/zimbra/id.txt
-for line in `cat /mnt/zimbra/id.txt`
+
+/usr/sbin/vzlist -a | grep running | awk '{print $1}' > id.txt
+for line in `cat id.txt`
 do
 ip=`/usr/sbin/vzlist -a | grep ${line} | awk '{print $4}'`
 zimbra=`/usr/sbin/vzctl exec ${line} "netstat -tupln" | grep 7071 |awk '{print $4}' | cut -d: -f2`
